bce25c1b6e35be0862eeb472478fd4af4a07625c
NeroPC <Nerocross507@gmail.com>
2024-09-13 10:33:43 +0300

    next try

--- tasker/urls.py
+++ tasker/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from tasker.apps import TaskerConfig
 from rest_framework.routers import SimpleRouter
 
-from tasker.views import TaskerViewSet, PublicTaskerListAPIView
+from tasker.views import TaskerViewSet, PublicTaskerListAPIView, FreeExecutorsListAPIView
 
 app_name = TaskerConfig.name
 
@@ -12,4 +12,5 @@ router.register("", TaskerViewSet, basename="tasker")
 
 urlpatterns = [
     path("public/", PublicTaskerListAPIView.as_view(), name="public"),
+    path("free/", FreeExecutorsListAPIView.as_view(), name="public"),
 ] + router.urls
