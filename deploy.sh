d6513f151ce3e4275ae5251ae41e5266f4387ae0
SRE <Nerocross507@gmail.com>
2024-09-18 12:37:47 +0300

    docker settings - ok try to restore db from backup

--- deploy.sh
+++ deploy.sh
@@ -9,3 +9,7 @@ docker-compose exec app python manage.py migrate
 
 #Если нужно создать суперпользователя (настройки переназначить можно в users/management/commands/csu.py)
 #docker-compose exec app python manage.py csu
+#Команда для сохранения дампа
+#python -Xutf8 manage.py dumpdata tasker users --natural-primary --natural-foreign  --exclude contenttypes --output fixtures/db.json --indent 4
+#Команда для загрузки фикстур
+#python manage.py loaddata fixtures/db.json
