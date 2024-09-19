02e1dac2995f863f8ba8bbc93f89dd2a80e89b45
SRE <Nerocross507@gmail.com>
2024-09-19 15:30:03 +0300

    remove custom logic of get_queryset to services.py

---
+++ tasker/services.py
@@ -0,0 +1,23 @@
+from django.db.models import Count
+
+from tasker.models import Task
+from users.models import User
+
+
+def free_executor_get_queryset(self):
+    user_min_task = (
+        User.objects.all()
+        .annotate(task_count=Count("tasks"))
+        .order_by("task_count")
+        .first()
+    )
+    task_parent = Task.objects.filter(parent_task__isnull=False, executors=None).first()
+    user_task_parent = User.objects.filter(tasks__id=task_parent.id).first()
+    count_user_task_parent = len(list(user_task_parent.tasks.all()))
+    count_user_min_task = len(list(user_min_task.tasks.all()))
+    if count_user_task_parent - count_user_min_task <= 2:
+        queryset = User.objects.filter(pk=user_task_parent.pk)
+    else:
+        queryset = User.objects.filter(pk=user_min_task.pk)
+
+    return queryset
--- tasker/views.py
+++ tasker/views.py
@@ -1,18 +1,16 @@
-from django.db.models import Count
 from django.utils.decorators import method_decorator
 from drf_yasg.utils import swagger_auto_schema
 from rest_framework import viewsets, generics
 from rest_framework.filters import SearchFilter, OrderingFilter
 from rest_framework.permissions import IsAuthenticated
 
+import tasker.services
 from tasker.models import Task
 from tasker.paginations import TaskPaginator
 from tasker.permissions import IsOwner
 from tasker.serializers import TaskSerializer, FreeExecutorsListSerializer, ImportantTaskListSerializer
-from users.models import User
 
 
-# TaskerViewSet, PublicTaskerListAPIView
 @method_decorator(
     name="list", decorator=swagger_auto_schema(operation_description="Список задач")
 )
@@ -57,17 +55,10 @@ class FreeExecutorsAPIView(generics.ListAPIView):
     serializer_class = FreeExecutorsListSerializer
 
     def get_queryset(self):
-        user_min_task = User.objects.all().annotate(task_count=Count('tasks')).order_by('task_count').first()
-        task_parent = Task.objects.filter(parent_task__isnull=False, executors=None).first()
-        user_task_parent = User.objects.filter(tasks__id=task_parent.id).first()
-        count_user_task_parent = len(list(user_task_parent.tasks.all()))
-        count_user_min_task = len(list(user_min_task.tasks.all()))
-        if count_user_task_parent - count_user_min_task <= 2:
-            queryset = User.objects.filter(pk=user_task_parent.pk)
-        else:
-            queryset = User.objects.filter(pk=user_min_task.pk)
+        queryset = tasker.services.free_executor_get_queryset(self.request.user)
 
         return queryset
+
 
 
 class ImportantTasksListAPIView(generics.ListAPIView):
